15b898bb1af71f2f6e55165ea1cb7d33e64ba07c
Ned McConnell <emcconnell@shapeup.com>
2015-04-03 15:36:12 -0400

    Adding fresh_tomatoes

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,4 +1,5 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
@@ -14,4 +15,12 @@ usual_suspects = media.Movie("The Usual Suspects",
                              "\"The greatest trick the devil ever pulled was convincing the world he didn't exist,\" says con man Kint (Kevin Spacey), drawing a comparison to the most enigmatic criminal of all time, Keyser Soze. Kint attempts to convince the feds that the mythic crime lord not only exists, but is also responsible for drawing Kint and his four partners into a multi-million dollar heist that ended with an explosion in San Pedro Harbor - leaving few survivors.",
                              "http://www.impawards.com/1995/posters/usual_suspects_ver2.jpg",
                              "https://www.youtube.com/watch?v=oiXdPolca5w")
-usual_suspects.show_trailer()
+
+school_of_rock = media.Movie("School of Rock",
+                             "",
+                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "")
+
+
+movies = [toy_story, avatar, usual_suspects, school_of_rock]
+fresh_tomatoes.open_movies_page(movies)
